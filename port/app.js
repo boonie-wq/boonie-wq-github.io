136ff939ca88d3286f0bc953fce9ea1d51be4c72
boonie-wq <138822219+boonie-wq@users.noreply.github.com>
2023-09-06 00:30:53 +0200

    Add files via upload

---
+++ port/app.js
@@ -0,0 +1,23 @@
+//Select a HTML Element in a variable.
+let myElement = document.querySelector("h1");
+
+console.log(myElement);
+//Prints the HTML Element to console
+ 
+//Interact to HTML Document.
+myElement.innerHTML = "Sanders";
+console.log(innerHTML);
+//Prints the HTML Element to console.
+
+//interact to HTML Document .
+myElement.style.color = "red";
+console.log(style.color);
+//Prints the HTML Element to console.
+ 
+
+
+
+
+
+
+
